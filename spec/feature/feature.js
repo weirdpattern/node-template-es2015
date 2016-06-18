4a3163bc816c2b83b5b4217bdb296a0945d9ddf4
weirdpattern <patricio@weirdpattern.com>
2016-06-18 13:18:43 -0500

    Improving spec example

---
+++ spec/feature/feature.js
@@ -0,0 +1,7 @@
+import test from 'tape';
+import feature from '../../lib/feature';
+
+test('example', (assert) => {
+  assert.ok(feature, 'simple test example');
+  assert.end();
+});
--- spec/index.js
+++ spec/index.js
@@ -6,3 +6,4 @@ require('babel-core/register');
 require('babel-polyfill');
 
 // keep requiring all your test files here
+require('./feature');
