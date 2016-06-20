58e64af11ca2aa4c131a39c58c12b72743991463
weirdpattern <patricio@weirdpattern.com>
2016-06-20 09:56:55 -0500

    Update the way test are loaded and executed

--- spec/feature/feature.js
+++ spec/feature/feature.js
@@ -1,7 +1,6 @@
-import test from 'tape';
-import feature from '../../lib/feature';
-
-test('example', (assert) => {
-  assert.ok(feature, 'simple test example');
-  assert.end();
-});
+export default function (test, feature) {
+  test('example', (assert) => {
+    assert.ok(feature, 'simple test example');
+    assert.end();
+  });
+}
--- spec/index.js
+++ spec/index.js
@@ -5,5 +5,60 @@
 require('babel-core/register');
 require('babel-polyfill');
 
-// keep requiring all your test files here
-require('./feature');
+const test = require('tape');
+const glob = require('glob');
+const path = require('path');
+
+/*
+ * @private
+ * @function
+ *
+ * Wraps the tape functionality (no more .end() at the end =))
+ *
+ * @param {string} title - the title of the test.
+ * @param {Function} callback - the actual test code.
+ */
+function executor (title, callback) {
+  test(title, (assert) => {
+    callback(assert);
+    assert.end();
+  });
+}
+
+// get all spec files and parse them
+glob('spec/**/*.js', { 'realpath': true, 'ignore': 'spec/index.js' }, function (err, files) {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  let suite;
+  let method;
+  let library;
+
+  files && files.forEach(function (filename) {
+    suite = require(filename);
+
+    if (suite && 'default' in suite) {
+      suite = suite['default'];
+
+      [ library, method ] = path.basename(filename, '.js').split('#');
+      library = path.relative(
+        __dirname,                 // current directory
+        path.join('lib',           // lib directory
+          path.relative(           // path to library from lib or spec folder
+            __dirname,
+            path.dirname(filename)
+          ),
+          library                  // library (js file)
+        )
+      );
+
+      if (method != null) {
+        suite(executor, require(library)[method]);
+      } else {
+        suite(executor, require(library));
+      }
+    }
+  });
+});
